b78eb70a3c6bf9c7db4102bf0d400b29b46905c2
D Cousins <dc346@mcs.le.ac.uk>
2018-10-30 16:45:00 +0000

    finished checkpoint

--- src/main/java/eMarket/controller/ItemController.java
+++ src/main/java/eMarket/controller/ItemController.java
@@ -3,7 +3,55 @@
  */
 package eMarket.controller;
 
-public class ItemController {
+import java.util.List;
 
-    
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import eMarket.EMarketApp;
+import eMarket.domain.Order;
+import eMarket.domain.OrderItem;
+
+@Controller
+@RequestMapping("/item")
+public class ItemController
+{
+	public static OrderItem getItem(List<OrderItem> itemList, int id)
+	{
+		for(OrderItem i : itemList)
+		{
+			if(i.getId() == id) return i;
+		}
+		return null;
+	}
+	
+	@RequestMapping(value = "/detail", method = RequestMethod.GET)
+    public String orderAdd(@RequestParam(value="orderId", required=true) int orderId,
+    @RequestParam(value="itemId", required=false, defaultValue="-1") int itemId,
+    Model model)
+    {
+		ItemFormDto itemFormDto = new ItemFormDto();
+    	if(itemId < 0)
+    	{
+    		itemFormDto.setId(OrderItem.lastId);
+    	}
+    	else
+    	{
+    		itemFormDto.setId(orderId);
+    		OrderItem thisItem = getItem(OrderController.getOrder(orderId).getItemList(), orderId);
+    		
+    		itemFormDto.setOrderId(orderId);
+    		itemFormDto.setAmount(thisItem.getAmount());
+    		itemFormDto.setProductId(thisItem.getProduct().getId());
+    	}
+    	
+    	itemFormDto.setProductList(EMarketApp.getStore().getProductList());
+    	
+    	model.addAttribute("itemFormDto", itemFormDto);
+    	
+    	return "form/itemDetail";
+    }
 }
--- src/main/java/eMarket/controller/OrderController.java
+++ src/main/java/eMarket/controller/OrderController.java
@@ -4,10 +4,10 @@ import java.util.ArrayList;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.WebDataBinder;
-import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
 import java.util.List;
 
 import eMarket.EMarketApp;
@@ -17,28 +17,56 @@ import eMarket.domain.Order;
 @RequestMapping("/order")
 public class OrderController
 {
-    @InitBinder
-    protected void initBinder(WebDataBinder binder)
-    {
-    		binder.addValidators(new DealValidator());
-    }
     
     //NEEDS LOGIC
     @RequestMapping(value = "/", method = RequestMethod.GET)
     public String main(Model model)
     {
-    	List<Order> temp = new ArrayList<>();
-    	temp.add(new Order());
-    	temp.get(0).setId();
-    	EMarketApp.getStore().setOrderList(temp);
     	model.addAttribute("orderList", EMarketApp.getStore().getOrderList());
 		
     	return "form/orderMaster";
     }
     
-    @RequestMapping(value = "/add", method = RequestMethod.GET)
-    public String orderAdd(Model model)
+    public static Order getOrder(int id)
     {
+    	for(Order o : EMarketApp.getStore().getOrderList())
+		{
+			if(o.getId() == id)
+			{
+				return o;
+			}
+		}
+    	return null;
+    }
+    
+    @RequestMapping(value = "/add", method = RequestMethod.GET)
+    public String orderAdd(@RequestParam(value="orderId", required=false, defaultValue="-1") int orderId,
+    Model model)
+    {
+    	Order order = null;
+    	if(orderId < 0)
+    	{
+    		order = new Order();
+    		order.setId();
+    		orderId = order.getId();
+    		EMarketApp.getStore().getOrderList().add(order);
+    	}
+    	else
+    	{
+    		order = getOrder(orderId);
+    	}
+    	
+    	model.addAttribute("order", order);
+    	
     	return "form/orderDetail";
+    }
+    
+    @RequestMapping(value = "/delete", method = RequestMethod.GET)
+    public String orderDelete(@RequestParam(value="orderId", required=false, defaultValue="-1") int orderId,
+    Model model)
+    {
+    	EMarketApp.getStore().getOrderList().remove(getOrder(orderId));
+    	
+    	return "form/orderMaster";
     }
 }
